e2e631db3e04e53398cd3b452180561412a72b8c
Logan Jones <44062917+LoganAJones@users.noreply.github.com>
2020-04-16 13:59:05 -0400

    functionality

---
+++ src/main/java/com/example/budgetingapplication/MainCreateBudgetActivity.java
@@ -0,0 +1,14 @@
+package com.example.budgetingapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCreateBudgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_new_budget);
+}
+}
