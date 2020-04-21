6e131b00134d0687028492ac9286d72585eee8b5
jessethomascs <56457691+jessethomascs@users.noreply.github.com>
2020-04-21 18:14:43 -0400

    Updated Class Organization
    Removed jesse.java and created new ViewBudget class to be in its place (which pre-existed but didn't quite work for some reason idk). Also fixed some stuff with PieChart

--- app/src/main/java/com/example/budgetingapplication/MainActivity.java
+++ app/src/main/java/com/example/budgetingapplication/MainActivity.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity { // HOME CLASS
-    private Button button;
+    private Button button; // FIXME: Whoever made these please make this more descriptive
     private Button button2;
 
     public static final String EXTRA_MESSAGE = "com.example.budgetingapplication.secondinstance";
@@ -57,81 +57,8 @@ public class MainActivity extends AppCompatActivity { // HOME CLASS
         startActivity(intent);
     }
 
-
-
-    public void JesseActivity(View view) {
-        Intent intent = new Intent(this, jesse.class);
-        //EditText editText = (EditText) findViewById(R.id.editText);
-        //String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, "fixme");
-        startActivity(intent);
-    }
-
     // Required things: a file stream of some sort, depends on the function we're in at the time.
     private static final String FILE_NAME = "example.txt";
     EditText mEditText;
 
-    public void LoadFile(View v) {
-        FileInputStream fis = null;
-
-        try {
-            fis = openFileInput(FILE_NAME);
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader br = new BufferedReader(isr);
-            StringBuilder sb = new StringBuilder();
-            String text;
-
-            while((text = br.readLine()) != null) {
-                sb.append(text).append("\n"); // Reads text line by line
-            }
-
-            mEditText.setText(sb.toString());
-
-            // debugging
-            System.out.println("testing");
-            for(int i = 0; i < sb.length(); i++) {
-                System.out.println("Current index: " + i + ", line is: " + sb.toString());
-            }
-
-            // debugging end
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public void Save(View v) {
-        String text = mEditText.getText().toString();
-        FileOutputStream fos = null;
-
-        try {
-            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
-            fos.write(text.getBytes());
-
-            mEditText.getText().clear();
-            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME, Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/budgetingapplication/ViewBudget.java
+++ app/src/main/java/com/example/budgetingapplication/ViewBudget.java
@@ -2,13 +2,54 @@ package com.example.budgetingapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewBudget extends AppCompatActivity {
+
+    float budgetInformation[] = {1.0f, 2.0f, 5.0f, 10.5f};
+    String numNames[] = {"Tax Bracket", "Car % From Budget", "Hershey's Bar", "CS:GO Skins"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jesse);
+
+        startPieChart();
+    }
+
+
+    public void startPieChart() {
+        // Pie Entries
+        List<PieEntry> pieEntries = new ArrayList<>();
+
+        for(int i = 0; i < budgetInformation.length; i++) {
+            pieEntries.add(new PieEntry(budgetInformation[i], numNames[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries, "Your Budget!");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setValueTextSize(20.0f);
+        PieData data = new PieData(dataSet);
+
+        // Chart
+        PieChart chart = (PieChart) findViewById(R.id.display_pie_chart);
+        chart.setData(data);
+        chart.setDrawEntryLabels(false);
+        chart.getDescription().setText("Your Budget");
+        chart.getDescription().setTextSize(15.0f);
+        chart.animateY(1000);
+        chart.invalidate();
     }
 }
